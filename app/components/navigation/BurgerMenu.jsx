c9dece2842d6675ac4752997cdef00d13d29e620
aleksey-savin <aleksey.s.savin@yandex.ru>
2024-07-18 01:22:53 +1000

    fixed error

--- app/components/navigation/BurgerMenu.jsx
+++ app/components/navigation/BurgerMenu.jsx
@@ -1,6 +1,6 @@
 const BurgerMenu = () => {
   return (
-    <div className="border-dashed border-r border-gray-200 h-[100%] hidden sm:flex lg:hidden shrink-0 items-center">
+    <div className="border-dashed border-r border-gray-200 h-[100%] hidden max-lg:flex lg:hidden shrink-0 items-center">
       <div className="px-[30px]">
         <img src="/images/burger-menu.svg" alt="" />
       </div>
