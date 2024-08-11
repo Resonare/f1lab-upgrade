0f11e14a731359f4635c90cda2d916b5fca5c578
aleksey-savin <aleksey.s.savin@yandex.ru>
2024-08-11 22:54:02 +1000

    fixed errors

---
+++ app/components/misc/ArrowRSecondary.jsx
@@ -0,0 +1,31 @@
+const ArrowRSecondary = () => {
+  return (
+    <span
+      className={`p-[14px] group-hover:p-5 group-hover:self-start transition-all duration-300`}
+    >
+      <svg
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M1.95996 9.95557L18.8697 9.95547"
+          stroke="#22282E"
+          strokeWidth="2"
+          strokeLinecap="round"
+        />
+        <path
+          d="M10.4043 1.51465L18.8895 9.99988L10.4042 18.4852"
+          stroke="#22282E"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </span>
+  );
+};
+
+export default ArrowRSecondary;
