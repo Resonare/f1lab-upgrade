937ef74804405d9df36711f39e4ea9a1cb64819d
aleksey-savin <aleksey.s.savin@yandex.ru>
2024-08-13 21:41:04 +1000

    testing another icons connection method

--- app/components/consulting/HeroV2.jsx
+++ app/components/consulting/HeroV2.jsx
@@ -12,23 +12,24 @@ const Hero = () => {
         бизнеса и не создает ненужных рисков?
       </ContentCard>
       <div className="row-start-2 row-end-3 col-start-2 col-end-3 flex items-end overflow-auto">
-        <img
-          height="85px"
-          className="max-lg:hidden h-1/2"
-          src="/images/misc/arrow-card-right.svg"
-          alt=""
-        />
+        <div className="grid grid-rows-2 grid-cols-2 w-full h-full">
+          <div className=""></div>
+          <div className=""></div>
+          <div className="lg:border-t lg:border-r lg:border-dashed lg:rounded-tr-2xl"></div>
+          <div className=""></div>
+        </div>
       </div>
       <ContentCard row={3} col={2}>
         ИТ-аудит F1 LAB, предоставит полный отчёт о состоянии и
         отказоустойчивости вашей ИТ-среды.
       </ContentCard>
       <div className="row-start-3 row-end-4 col-start-1 col-end-2 flex items-end justify-end">
-        <img
-          className="max-lg:hidden"
-          src="/images/misc/arrow-card-left.svg"
-          alt=""
-        />
+        <div className="grid grid-rows-2 grid-cols-2 w-full h-full">
+          <div className=""></div>
+          <div className="lg:border-b lg:border-l lg:border-dashed lg:rounded-bl-2xl"></div>
+          <div className=""></div>
+          <div className=""></div>
+        </div>
       </div>
       <div className="row-start-4 max-lg:mt-60 col-start-1 col-end-5 ">
         <PrimaryButton type="accent" row={4} col={1}>
