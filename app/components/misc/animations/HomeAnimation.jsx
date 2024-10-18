5de0edb27572fe12fd157baec47d0b3ddd2b52bf
Resonare <47446377+Resonare@users.noreply.github.com>
2024-10-18 11:57:28 +1000

    added animation smooth appearance

--- app/components/misc/animations/HomeAnimation.jsx
+++ app/components/misc/animations/HomeAnimation.jsx
@@ -1,16 +1,26 @@
+import { useState } from "react";
 import { useLottie } from "lottie-react";
 
 import homeAnimationJson from "./home-animation.json";
 
 const HomeAnimation = () => {
+  const [loaded, setLoaded] = useState(false);
+
   const options = {
     animationData: homeAnimationJson,
     loop: true,
+    onDOMLoaded: () => {
+      setLoaded(true);
+    },
   };
 
   const { View } = useLottie(options);
 
-  return <>{View}</>;
+  return (
+    <div className={`${!loaded && `opacity-0`} transition-all duration-500`}>
+      {View}
+    </div>
+  );
 };
 
 export default HomeAnimation;
