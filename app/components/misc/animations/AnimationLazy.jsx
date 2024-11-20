a65133c2c4a2941418320dc0f86352a755b7d3f2
Resonare <47446377+Resonare@users.noreply.github.com>
2024-11-20 14:17:32 +1000

    fixed home animation

--- app/components/misc/animations/AnimationLazy.jsx
+++ app/components/misc/animations/AnimationLazy.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useLottie } from "lottie-react";
 
-const AnimationLazy = ({ data, hero = false }) => {
+const AnimationLazy = ({ className, data, hero = false }) => {
   const [loaded, setLoaded] = useState(false);
 
   const options = {
@@ -23,20 +23,10 @@ const AnimationLazy = ({ data, hero = false }) => {
     }
   }, [animationContainerRef]);
 
-  const widthClasses =
-    "xl:[&>div>svg]:w-[1950px] lg:[&>div>svg]:w-[1550px] md:[&>div>svg]:w-[1300px]";
-
-  const translateClasses =
-    "xl:[&>div>svg>g]:translate-y-[5%] lg:[&>div>svg>g]:translate-y-[16%] sm:[&>div>svg>g]:translate-y-[76%]";
-
   return (
     <div
-      className={`${!loaded && `opacity-0`} ${hero && widthClasses} ${
-        hero && translateClasses
-      } ${
-        hero
-          ? `[&>div>svg]:right-0 [&>div>svg]:h-full`
-          : `[&>div>svg]:left-1/2 [&>div>svg]:-translate-x-1/2`
+      className={`${className} ${!loaded && `opacity-0`} ${
+        !hero && `[&>div>svg]:left-1/2 [&>div>svg]:-translate-x-1/2`
       } [&>div>svg>g]:no-clip [&>div>svg]:w-full [&>div>svg]:h-full [&>div>svg]:absolute transition-all duration-500 overflow-hidden w-full h-full [&>div]:h-full [&>div]:relative`}
     >
       {View}
